a8d92144128a805508391acb7aab64d9150c762c
madhu-addanki <120074676+madhu-addanki@users.noreply.github.com>
2023-06-05 12:04:18 +0530

    Add files via upload

---
+++ vaman/iot/code/code3/src/main.cpp
@@ -0,0 +1,57 @@
+//----------------------Skeleton Code--------------------//
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+//    Can be client or even host   //
+#ifndef STASSID
+#define STASSID "vivo1726"  // Replace with your network credentials
+#define STAPSK  "salma1234"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+void OTAsetup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(5000);
+    ESP.restart();
+  }
+  ArduinoOTA.begin();
+}
+
+void OTAloop() {
+  ArduinoOTA.handle();
+}
+
+//-------------------------------------------------------//
+
+void setup(){
+  OTAsetup();
+
+  //-------------------//
+  // Custom setup code //
+  //-------------------//
+	pinMode(2, OUTPUT);
+	pinMode(4, OUTPUT);
+
+}
+
+void loop() {
+  OTAloop();
+  delay(10);  // If no custom loop code ensure to have a delay in loop
+  //-------------------//
+  // Custom loop code  //
+  //-------------------//
+	int P,Q,C,R;
+	P=0;
+	Q=1;
+	C=((!(P))&&(!(!Q)))||((!(!P))&&(!Q));
+	R=((P&&(!Q))||((!P)&&Q));
+	digitalWrite(2,C);
+	digitalWrite(4,R);
+}
+
+
